ad488040f122aac29383cbd8ac5166cd47c666a9
Re9iNee <attarzadeh76@gmail.com>
2022-12-07 12:52:32 +0330

    Posts complete

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,7 +1,10 @@
 import Header from "@components/Header";
-import { sanityClient } from "@sanityConfig";
+import { sanityClient, urlFor } from "@sanityConfig";
 import { Post } from "@types";
-import { GetStaticPaths, GetStaticPathsResult, GetStaticProps } from "next";
+import { GetStaticPathsResult, GetStaticProps } from "next";
+import { ReactNode } from "react";
+import PortableText from "react-portable-text";
+import serializers from "./serializerObject";
 
 type Props = {
     post: Post;
@@ -12,12 +15,56 @@ const Post = ({ post }: Props) => {
     return (
         <main>
             <Header />
+
+            <img
+                className='w-full h-40 object-cover'
+                src={urlFor(post.mainImage).url()}
+                alt=''
+            />
+
+            <article className='max-w-3xl mx-auto p-5'>
+                <h1 className='text-3xl mt-10 mb-3'>{post.title}</h1>
+                <h2 className='text-xl font-light text-gray-500 mb-2'>
+                    {post.description}
+                </h2>
+
+                <div className='flex items-center space-x-2'>
+                    <img
+                        className='h-10 w-10 rounded-full'
+                        src={urlFor(post.author.image).url()}
+                        alt=''
+                    />
+
+                    <p className='font-extralight text-sm'>
+                        Blog post by{" "}
+                        <span className='text-green-600'>
+                            {post.author.name}
+                        </span>
+                        - Published at
+                        {new Date(post._createdAt).toLocaleString()}
+                    </p>
+                </div>
+
+                <div>
+                    <div className='mt-10'>
+                        <PortableText
+                            serializers={serializers}
+                            projectId={
+                                process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+                            }
+                            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+                            content={post.body}
+                        />
+                    </div>
+                </div>
+            </article>
         </main>
     );
 };
 
 export default Post;
 
+// to use ISR - we gotta get all slugs and paths to the posts in order to cache them.
 export const getStaticPaths = async (): Promise<GetStaticPathsResult> => {
     // Fetch All Posts
     const query = `//groq
