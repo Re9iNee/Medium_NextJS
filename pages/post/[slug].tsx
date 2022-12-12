dedfbfcc53ba909f7e408f3b75dcad0798f5c4c7
Re9iNee <attarzadeh76@gmail.com>
2022-12-12 12:02:28 +0330

    remove serializers object file

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -6,8 +6,26 @@ import { sanityClient, urlFor } from "@sanityConfig";
 import { Post } from "@types";
 import { GetStaticPathsResult, GetStaticProps } from "next";
 import Image from "next/image";
+import { ReactNode } from "react";
 import PortableText from "react-portable-text";
-import serializers from "./serializerObject";
+
+// This is a helper object to override how we want PortableText to render different elements
+const serializers = {
+    h1: (props: object[]) => (
+        <h1 className='text-2xl font-bold my-5' {...props} />
+    ),
+    h2: (props: object[]) => (
+        <h2 className='text-xl font-bold my-5' {...props} />
+    ),
+    li: ({ children }: { children: ReactNode }) => (
+        <li className='ml-4 list-disc'>{children}</li>
+    ),
+    link: ({ href, children }: { href: string; children: ReactNode }) => (
+        <a href={href} className='tex-blue-500 hover:underline'>
+            {children}
+        </a>
+    ),
+};
 
 type Props = {
     post: Post;
