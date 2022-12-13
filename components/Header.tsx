800d6b2a5c6522701fc09b6082627be37fe26ae8
Re9iNee <attarzadeh76@gmail.com>
2022-12-13 12:38:29 +0330

    Ref-using next image component for logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
+import mediumLogo from "../public/icons/logo.png";
 
 function Header() {
     return (
         <header className='flex justify-between p-5 max-w-7xl mx-auto'>
             <div className='flex items-center space-x-5'>
                 <Link href='/'>
-                    <picture>
-                        <img
-                            className='w-44 object-contain cursor-pointer'
-                            src='/icons/logo.png'
-                            alt='Medium Logo'
-                        />
-                    </picture>
+                    <Image
+                        src={mediumLogo}
+                        alt='Medium Logo'
+                        className='w-44 object-contain cursor-pointer'
+                    />
                 </Link>
                 <div className='hidden md:inline-flex items-center space-x-5'>
                     <h3>About</h3>
