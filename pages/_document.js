08afb9b7b92a7fc007431a1d4b96f9639c1a63d3
Re9iNee <attarzadeh76@gmail.com>
2022-12-13 12:25:46 +0330

    REF-setting default language to html

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+import React from "react";
+
+const _document = () => {
+    return (
+        <Html lang='en'>
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+};
+
+export default _document;
